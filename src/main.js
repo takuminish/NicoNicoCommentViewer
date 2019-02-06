90707eb314bf78bc76382bfe646d19dcd98fec45
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-02-07 00:49:49 +0900

    css作成

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ const {app, BrowserWindow} = require('electron');
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({
+	width: 800,
+	height: 600
+    });
     mainWindow.loadFile('index.html');
 
     mainWindow.on('closed', () => {
