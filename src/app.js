0fe5a1b8b1858cc4d94c6b2fdc3f2f16914eb7b9
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-02-09 15:52:26 +0900

    ボタンクリックでコメント作成&移動開始

--- src/app.js
+++ src/app.js
@@ -1,22 +1,33 @@
 window.onload = function() {
 
-    document.getElementById("button1").onclick = function() {commentMove("comment1");}
-    document.getElementById("button2").onclick = function() {commentMove("comment2");}
-    document.getElementById("button3").onclick = function() {commentMove("comment3");}
+    document.getElementById("button1").onclick = function() {commentMove(commentCreate("comment1"));}
+    document.getElementById("button2").onclick = function() {commentMove(commentCreate("comment2"));}
+    document.getElementById("button3").onclick = function() {commentMove(commentCreate("comment3"));}
      
 }
 
-    function commentMove(Id) {
-        let comment = document.getElementById(Id);
-        let dx = comment.offsetLeft;
-        let x = comment.offsetWidth;
+function commentMove(comment) {
+    let dx = comment.offsetLeft;
+    let x = comment.offsetWidth;
 
-	let time = setInterval( function() {
-	    comment.style.left = dx + "px";
-	    dx -= 1;
-	    if ((x * -1) > dx) {
-		clearInterval(time);
-	        comment.parentNode.removeChild(comment)
-	    }
-        }, 10);
-    }
+    let time = setInterval( function() {
+	comment.style.left = dx + "px";
+	dx -= 1;
+	if ((x * -1) > dx) {
+	    clearInterval(time);
+	    comment.parentNode.removeChild(comment)
+	}
+    }, 10);
+}
+
+function commentCreate(text) {
+    let comment = document.createElement("h1");
+    comment.textContent = text;
+    comment.setAttribute("id",text);
+    comment.setAttribute("class","comment");
+
+    document.body.appendChild(comment);
+    return comment;
+}
+
+
