4be3d439b30bc75d5f8002cad66acdeed48def71
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-02-10 17:42:25 +0900

    POSTメソッドの追加

--- app/server.js
+++ app/server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const server = express();
 
 console.log(__dirname)
+
 server.set('views', __dirname + '/views');
 server.set('view engine', "ejs")
 server.use('/static', express.static(__dirname + '/public'));
+
 server.get('/', (req,res) => {
     res.render("index");
 });
+
+server.post('/', (req,res) => {
+    res.send(res);
+});
+
+
+
 server.listen(3000);
 
 module.exports = server; 
