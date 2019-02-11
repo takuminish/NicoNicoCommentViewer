9f28d88e1b25a259a3d88eca72cb6bca33378cbe
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-02-11 19:36:00 +0900

    fontカラーを変更できるようにした

--- app/public/javascript/app.js
+++ app/public/javascript/app.js
@@ -17,13 +17,14 @@ class CommentArray {
 }
 
 class Comment {
-    constructor(id,text, top, fontSize) {
+    constructor(id,text, top, fontSize, fontColor) {
 	this.element = document.createElement("span");
 	this.element.textContent = text;
         this.element.setAttribute("id",id);
         this.element.setAttribute("class","comment");
 	this.element.style.top = `${top}px`;
 	this.element.style.fontSize = `${fontSize}px`;
+	this.element.style.color = fontColor;
 	this.is_alive = true;
     }
 
@@ -63,7 +64,7 @@ window.onload = () => {
 	    console.log(body);
 	});
 	let top = Math.floor( Math.random() * (600 + 1 - 0) ) + 0 ;
-	let comment2 = new Comment("comment2","<script>alert(1);</script>",top, 30);
+	let comment2 = new Comment("comment2","<script>alert(1);</script>",top, 30, "red");
 	commentArray.commentAdd(comment2);
 	comment2.viewFromBody();
 	comment2.Move();
